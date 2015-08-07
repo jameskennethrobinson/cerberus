ece511ac0faba732e0091fd4969f48d52746939d
James Robinson <jameskennethrobinson@gmail.com>
2015-08-07 10:42:47 -0700

    Update config.js

--- backend/server/config.js
+++ backend/server/config.js
@@ -6,18 +6,12 @@ var controller = require('./controller.js');
 var crudUtils = require('./utils/crudUtils.js');
 var path = require('path');
 
-
 var app = express();
 
-//middleware
-//app.use(cors());
-//app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../../client'));
 
 app.get('/', controller.sendIndex);
-//these routes are for TESTING ONLY
 app.get('/fetch', controller.sendSurfSpots);
-//populates db with one-time beach data (e.g. lat/long, etc)
 
 module.exports = app;
